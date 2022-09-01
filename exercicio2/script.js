be862f9558e66746cb04b0497d67e2371d40e6f3
heyalearaujo <alexandrearza@gmail.com>
2022-09-01 12:21:51 -0300

    Resolução exercicio 2 - Arrays

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+let arrayUm = [13 , 24 , 7]
+let arrayDois = ["olá", "Hola", "Hi"]
+let arrayTres = ["olá", 13, true]
+console.log(arrayUm.length)
+console.log(arrayDois.length)
+console.log(arrayTres.length)
+console.log(arrayUm[0])
+console.log(arrayDois[1])
+console.log(arrayTres[2])
+console.log(arrayTres.includes(13))
+console.log(arrayTres.includes("13"))
